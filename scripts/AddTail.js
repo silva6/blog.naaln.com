fe31aa28fdadc358d807ca314ca978734073045a
liszd <liamzhuce@gmail.com>
2016-12-29 23:22:43 -0500

    2016设计年鉴（三）

--- scripts/AddTail.js
+++ scripts/AddTail.js
@@ -2,11 +2,11 @@ hexo.extend.filter.register('before_post_render', function(data){
 
   var file_content = '<div style="clear:both"></div>';
   file_content += '<div class="cc">';
-  file_content += 'The [Why·Liam·Blog](https://blog.naaln.com) by [WhyLiam](https://www.naaln.com) is licensed under a [Creative Commons BY-NC-ND 4.0 International License](http://creativecommons.org/licenses/by-nc-nd/4.0/).\n\n';
-  file_content += '由[WhyLiam](https://www.naaln.com)创作并维护的[Why·Liam·Blog](https://blog.naaln.com)采用创作共用[保留署名-非商业-禁止演绎4.0国际许可证](http://creativecommons.org/licenses/by-nc-nd/4.0/)。\n\n';
-  file_content += '本文首发于[Why·Liam·Blog (https://blog.naaln.com)](https://blog.naaln.com)，版权所有，侵权必究。\n\n';
-  file_content += '本文永久链接：['+data.permalink+']('+data.permalink+')';
-  file_content += '</div>'
+  file_content += '<p>The <a href="https://blog.naaln.com">Why·Liam·Blog</a> by <a href="https://www.naaln.com" target="_blank" rel="external">WhyLiam</a> is licensed under a <a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="external">Creative Commons BY-NC-ND 4.0 International License</a>.</p>';
+  file_content += '<p>由<a href="https://www.naaln.com" target="_blank" rel="external">WhyLiam</a>创作并维护的<a href="https://blog.naaln.com">Why·Liam·Blog</a>采用创作共用<a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="external">保留署名-非商业-禁止演绎4.0国际许可证</a>。</p>';
+  file_content += '<p>本文首发于<a href="https://blog.naaln.com">Why·Liam·Blog (https://blog.naaln.com)</a>，版权所有，侵权必究。</p>';
+  file_content += '<p>本文永久链接：<a href="'+data.permalink+'">'+data.permalink+'</a><p>';
+  file_content += '</div>';
 
   try {
     if (file_content && data.content.length > 50) {
